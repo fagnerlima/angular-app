ff347cd5513f3c8424c04fc6a18cc995b5bc172f
Fagner Lima <contato@fagnerlima.pro.br>
2019-11-12 21:17:41 -0300

    Atribui permissão total para ROLE_ADMIN

--- src/app/security/role.guard.ts
+++ src/app/security/role.guard.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 
 import { AuthService } from './shared/auth.service';
+import { Role } from './shared/role.enum';
 
 @Injectable()
 export class RoleGuard implements CanActivate {
@@ -14,7 +15,7 @@ export class RoleGuard implements CanActivate {
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
     const expectedRole = route.data.expectedRole;
 
-    if (!this.authService.hasRole(expectedRole)) {
+    if (!this.authService.hasRole([expectedRole, Role.ADMIN])) {
       this.router.navigate(['erro-401']);
 
       return false;
