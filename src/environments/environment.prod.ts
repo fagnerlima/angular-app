84f5d04d3e58971fcd56a591ac00334c468453f1
Fagner Lima <contato@fagnerlima.pro.br>
2019-12-16 14:35:46 -0300

    Atualiza environment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,10 @@
 export const environment = {
   production: true,
   applicationName: 'Angular App',
+  oauth2: {
+    headers: {
+      authorization: 'Basic YW5ndWxhcjphbmd1bGFy'
+    }
+  },
   apiAuthUrl: 'http://localhost:8080'
 };
