4f554914840e92f93a4e84dd750be53aea245529
Fagner Lima <contato@fagnerlima.pro.br>
2020-04-28 20:48:51 -0300

    Adiciona tratamento de erros do OAuth2

--- src/app/core/shared/error-handler.service.ts
+++ src/app/core/shared/error-handler.service.ts
@@ -6,9 +6,25 @@ import { AppRoute } from '@app/shared/enum/app-route.enum';
 import { ToastService } from '@app/shared/service/toast.service';
 import { environment } from '@env/environment';
 import { HttpStatus } from '@app/shared/enum/http-status.enum';
+import { isObject } from '@app/shared/util/utils';
+import { OAuth2Error } from '@app/shared/enum/oauth2-error.enum';
 
 @Injectable()
 export class ErrorHandlerService {
+
+  private readonly oauth2ErrorMessages = new Map([
+    [OAuth2Error.INVALID_REQUEST, 'Requisição inválida'],
+    [OAuth2Error.INVALID_CLIENT, 'Cliente inválido'],
+    [OAuth2Error.INVALID_GRANT, 'Credenciais inválidas'],
+    [OAuth2Error.INVALID_SCOPE, 'Escopo inválido'],
+    [OAuth2Error.INVALID_TOKEN, 'Token inválido'],
+    [OAuth2Error.INSUFFICIENT_SCOPE, 'Escopo insuficiente'],
+    [OAuth2Error.UNAUTHORIZED, 'Acesso não autorizado'],
+    [OAuth2Error.UNAUTHORIZED_CLIENT, 'Cliente não autorizado'],
+    [OAuth2Error.UNSUPPORTED_GRANT_TYPE, 'Tipo de credenciais não suportado'],
+    [OAuth2Error.UNSUPPORTED_RESPONSE_TYPE, 'Tipo de resposta não suportado'],
+    [OAuth2Error.ACCESS_DENIED, 'Acesso negado'],
+  ]);
 
   constructor(
     private toastService: ToastService,
@@ -28,17 +44,28 @@ export class ErrorHandlerService {
   }
 
   private printMessages(httpErrorResponse: HttpErrorResponse): void {
-    const errors: string[] = httpErrorResponse.error.errors;
+    const error: any = httpErrorResponse.error;
 
-    if (!errors) {
+    if (!error) {
       this.printGenericMessage();
       return;
     }
 
-    errors.forEach(error => this.toastService.open(error, 'Erro'));
+    if (this.isOAuth2Error(error)) {
+      this.toastService.open(this.oauth2ErrorMessages.get(error['error']), 'Erro');
+      return;
+    }
+
+    this.toastService.open(error, 'Erro');
   }
 
   private printGenericMessage(): void {
     this.toastService.open('Ocorreu um erro durante a solicitação', 'Erro');
+  }
+
+  private isOAuth2Error(error: any): boolean {
+    return isObject(error)
+      && Object.keys(error).includes('error')
+      && Object.values(OAuth2Error).includes(error['error']);
   }
 }
---
+++ src/app/shared/enum/oauth2-error.enum.ts
@@ -0,0 +1,13 @@
+export enum OAuth2Error {
+  INVALID_REQUEST = 'invalid_request',
+  INVALID_CLIENT = 'invalid_client',
+  INVALID_GRANT = 'invalid_grant',
+  INVALID_SCOPE = 'invalid_scope',
+  INVALID_TOKEN = 'invalid_token',
+  INSUFFICIENT_SCOPE = 'insufficient_scope',
+  UNAUTHORIZED = 'unauthorized',
+  UNAUTHORIZED_CLIENT = 'unauthorized_client',
+  UNSUPPORTED_GRANT_TYPE = 'unsupported_grant_type',
+  UNSUPPORTED_RESPONSE_TYPE = 'unsupported_response_type',
+  ACCESS_DENIED = 'access_denied'
+}
